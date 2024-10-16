5b5e94427533f38ba7560832f0b3551fcbf801ab
Toma Vladut <tovlamy26@gmail.com>
2024-10-16 20:15:12 +0300

    revised the 1st seminar

--- sem01/simpleServer/server.js
+++ sem01/simpleServer/server.js
@@ -2,8 +2,12 @@ import express from 'express'
 
 const app = express()
 
-app.get('/hello', (req,res)=>{
-    res.send('hello world!')
+app.get('/', (req,res)=>{
+    res.send('hello web!')
+})
+
+app.get('/ping', (req, res)=>{
+    res.send('pong')
 })
 
 app.listen(8080)
